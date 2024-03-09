95084a3f1b9b86d10a62357460c05e578800965c
justin <justinreyes13003@gmail.com>
2024-03-10 00:07:50 +0800

    feat: listing restrooms when clicking on a building

--- src/pages/BuildingsList.tsx
+++ src/pages/BuildingsList.tsx
@@ -1,20 +1,39 @@
 import { useQuery } from 'react-query';
 import { Buildings } from '../api/requests/Buildings';
 import Review from '../components/Review';
+import { Rating } from 'react-simple-star-rating';
+import { useState } from 'react';
 export default function BuildingsList() {
-  const { data } = useQuery('buildings', Buildings.getAll);
-  console.log(data);
+  const { data: buildings } = useQuery('buildings', Buildings.getAll);
+  const [selectedBuilding, setSelectedBuilding] = useState(null);
+
+  const restrooms = selectedBuilding?.restrooms;
+
   return (
     <>
       <div className='buildings-list main'>
-        <div className='buildings-list search'>Sample Search Bar</div>
-        <div className='buildings-list item'>Building</div>
-        <div className='buildings-list item'>Building</div>
-        <div className='buildings-list item'>Building</div>
-        <div className='buildings-list item'>Building</div>
-        <div className='buildings-list item'>Building</div>
-        <Review author={'mama mo'} score={2.41} text={'bruh'} />
+        {buildings?.map((building) => (
+          <div
+            className='buildings-list item'
+            key={building.id}
+            onClick={() => setSelectedBuilding(building)}
+          >
+            {building.name}
+          </div>
+        ))}
       </div>
+      {/* <Review author={'mama mo'} score={2.41} text={'bruh'} /> */}
+      {restrooms &&
+        restrooms?.map((restroom) => (
+          <div className='restroom-list' key={restroom.id}>
+            <span className='restroom-name'>{restroom.name}</span>
+            <Rating
+              readonly={true}
+              initialValue={restroom.rating}
+              allowFraction={true}
+            />
+          </div>
+        ))}
     </>
   );
 }
