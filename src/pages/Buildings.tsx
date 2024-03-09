fb6128f545af5fb31ed4a20cbd75b667aa8ef9d4
justin <justinreyes13003@gmail.com>
2024-03-10 02:28:35 +0800

    functional: map highlighting, showing details

--- src/pages/Buildings.tsx
+++ src/pages/Buildings.tsx
@@ -1,9 +1,21 @@
 import CampusMap from '../components/CampusMap';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Buildings as BuildingApi } from '../api/requests/Buildings';
 
 export default function Buildings() {
+  const [selectedBuilding, setSelectedBuilding] = useState(null);
+  const { data: buildings } = useQuery('buildings', BuildingApi.getAll);
+
+  const buildingDetails = buildings?.filter(
+    (building) => building.name === selectedBuilding,
+  );
+  console.log(buildingDetails);
+
   return (
     <>
-      <CampusMap />
+      <CampusMap setSelectedBuilding={setSelectedBuilding} />
+      {JSON.stringify(buildingDetails)}
     </>
   );
 }
