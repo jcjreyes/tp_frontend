3a1993d8d8802c8a5124b0e2c01479fdf71cbf28
justin <justinreyes13003@gmail.com>
2024-03-11 01:51:04 +0800

    chore: cleanup of selected building in map view

--- src/pages/Buildings.tsx
+++ src/pages/Buildings.tsx
@@ -2,20 +2,51 @@ import CampusMap from '../components/CampusMap';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { Buildings as BuildingApi } from '../api/requests/Buildings';
+import { Rating } from 'react-simple-star-rating';
+
+interface Restroom {
+  id: string;
+  name: string;
+  description: string;
+  rating: number;
+}
 
 export default function Buildings() {
   const [selectedBuilding, setSelectedBuilding] = useState(null);
   const { data: buildings } = useQuery('buildings', BuildingApi.getAll);
 
-  const buildingDetails = buildings?.filter(
+  const buildingDetails = buildings?.find(
     (building) => building.name === selectedBuilding,
   );
-  console.log(buildingDetails);
+
+  const restrooms: Restroom[] = buildingDetails?.restrooms;
 
   return (
     <>
       <CampusMap setSelectedBuilding={setSelectedBuilding} />
-      {JSON.stringify(buildingDetails)}
+      {buildingDetails && (
+        <aside className='sidebar'>
+          <div className='building-details'>
+            <span className='building-name'>{buildingDetails?.name}</span>
+            <span className='building-desc'>
+              {buildingDetails?.description}
+            </span>
+          </div>
+          <div className='restroom-list'>
+            {restrooms &&
+              restrooms.map((restroom) => (
+                <div className='restroom-item' key={restroom.id}>
+                  <span className='restroom-name'>{restroom.name}</span>
+                  <Rating
+                    readonly={true}
+                    initialValue={restroom?.rating}
+                    allowFraction={true}
+                  />
+                </div>
+              ))}
+          </div>
+        </aside>
+      )}
     </>
   );
 }
