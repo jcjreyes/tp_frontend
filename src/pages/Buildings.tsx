8b5815a414b6e1e44dba1e74d20683e85d3d587f
Anthony <anthony.bicomong@obf.ateneo.edu>
2024-03-24 15:08:52 +0800

    added css for buttons

--- src/pages/Buildings.tsx
+++ src/pages/Buildings.tsx
@@ -29,40 +29,42 @@ export default function Buildings() {
       </div>
 
       <aside className={'sidebar open'}>
-        <div className='building-picture'></div>
+        <div className='building-picture'>
+          <img src="pic.jpg" alt="Image not found" width="100%" height="100%"></img>
+        </div>
         <div className='building-details'>
           <span className='building-name'>{"Cilantro Eastwood"}</span>
           <span className='building-desc'>{"(Description) Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"}</span>
         </div>
-
+        <center> <h2> Toilets Available: </h2> </center>
         <div className='restroom-list'>
-          <div className="column">
-            <button className="button">
-              <div className="button-title">RESTROOM</div>
-              <div className="button-details">
-                <span className="button-rating"> * STARS * </span>
-                <span className="button-distance">100m</span>
-              </div>
-            </button>
-          </div>
-          <div className="column">
-            <button className="button">
-              <div className="button-title">BANYO</div>
-              <div className="button-details">
-                <span className="button-rating"> * STARS * </span>
-                <span className="button-distance">100m</span>
-              </div>
-            </button>
-          </div>
-          <div className="column">
-            <button className="button">
-              <div className="button-title">OUT OF ORDER</div>
-              <div className="button-details">
-                <span className="button-rating"> * STARS * </span>
-                <span className="button-distance">100m</span>
-              </div>
-            </button>
-          </div>
+          <button className="toilet-button">
+            <div className="frame">
+              <div className="text"> Insert Text Here</div>
+                <div className="div">
+                  <img className="stars" alt="Frame" src="frame-1.svg" />
+                  <div className="text-wrapper">183m</div>
+                </div>
+            </div>
+          </button>
+          <button className="toilet-button">
+            <div className="frame">
+              <div className="text"> Bathroom Here </div>
+                <div className="div">
+                  <img className="stars" alt="Frame" src="frame-1.svg" />
+                  <div className="text-wrapper">183m</div>
+                </div>
+            </div>
+          </button>
+          <button className="toilet-button">
+            <div className="frame">
+              <div className="text"> Another Banyo </div>
+                <div className="div">
+                  <img className="stars" alt="Frame" src="frame-1.svg" />
+                  <div className="text-wrapper">183m</div>
+                </div>
+            </div>
+          </button>
         </div>
       </aside>
     </>
