b5942155fb2f4b6c6b701d05fbd3824996ebac07
justin <justinreyes13003@gmail.com>
2024-03-25 18:12:04 +0800

    feat: display available reviews for a bathroom when clikced

--- src/components/RestroomDetails.tsx
+++ src/components/RestroomDetails.tsx
@@ -1,5 +1,6 @@
 import { Restrooms } from '../api/requests/Restrooms';
 import AddReview from './AddReview';
+import Review from './Review';
 import { useQuery } from 'react-query';
 import { useState } from 'react';
 
@@ -8,9 +9,14 @@ export default function RestroomDetails({ restroom }) {
     data: details,
     isLoading,
     isError,
-  } = useQuery('restrooms', () => Restrooms.getOne(restroom?.id));
+  } = useQuery(['restroom', restroom?.id], () =>
+    Restrooms.getOne(restroom?.id),
+  );
 
   const [showReviewForm, setShowReviewForm] = useState(false);
+  const [hasReview, setHasReview] = useState(false);
+  const reviews = details?.reviews;
+
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -20,9 +26,17 @@ export default function RestroomDetails({ restroom }) {
     return <span>Error occurred...</span>;
   }
 
-
   return (
     <>
+      {console.log(details)}
+      {reviews &&
+        reviews.map((review) => (
+          <Review
+            author={review.author}
+            score={review.rating.rating}
+            text={review.content}
+          />
+        ))}
       <div className='dropdown'>
         <button onClick={() => setShowReviewForm(!showReviewForm)}>
           {showReviewForm ? 'Hide Review Form' : 'Add Review'}
