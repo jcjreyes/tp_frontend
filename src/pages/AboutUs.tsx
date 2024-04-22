ed76852cd95140896438642cc7cdf5b5255e93fb
Anthony <anthony.bicomong@obf.ateneo.edu>
2024-04-22 15:36:59 +0800

    styled about us page, added our team

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -6,11 +6,41 @@ export default function AboutUs() {
       <div className='aboutus-container'>
         <div className='aboutus-content'>
           <h2>About Us</h2>
-          <p>Welcome to TaePare! We are a team of passionate individuals dedicated to providing you with the best experience possible.</p>
+          <p>Welcome to TaePare!</p>
           <p>Our mission is to create innovative solutions that meet the needs of our users and exceed their expectations.</p>
           <p>At TaePare, we value transparency, integrity, and collaboration. We believe in building strong relationships with our customers and partners.</p>
+
+          <h2>Our Team</h2>
+          <div className="team-cards-container">
+            <div className="team-member">
+              <img src="member1.jpg" alt="Member pic"></img>
+              <h2>John Doe</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="team-member">
+              <img src="member1.jpg" alt="Member pic"></img>
+              <h2>John Doe</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="team-member">
+              <img src="member1.jpg" alt="Member pic"></img>
+              <h2>John Doe</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="team-member">
+              <img src="member1.jpg" alt="Member pic"></img>
+              <h2>John Doe</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="team-member">
+              <img src="member1.jpg" alt="Member pic"></img>
+              <h2>John Doe</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
         </div>
       </div>
+
     </>
     
   );
